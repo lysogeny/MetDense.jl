0881db73c58d5179328f684d631529bf7b8bc1a4
Sveta <s.ovchinnikova@zmbh.uni-heidelberg.de>
2022-05-24 16:34:23 +0200

    Can read in all the basic info

--- read_metdense.jl
+++ read_metdense.jl
@@ -1,4 +1,3 @@
-
 @enum MethCall ::UInt32 begin
     nocall = 0x00
     unmeth = 0x01
@@ -8,52 +7,53 @@ end
 
 struct GenomicPosition
     chrom :: String
-    pos :: Int32
+    pos :: UInt32
 end
 
 struct GenomicInterval
     chrom :: String
-    iv :: Range{Int32}
+    iv :: Range{UInt32}
 end
 
 struct MetDenseFile
     f ::IOStream
     cell_names ::Vector{String}
-    chroms_dict ::Dict{ String, UnitRange{UInt32} }
-    offset_data_block ::UInt32
+    chroms_dict ::Dict{ String, StepRange{UInt64} }
+    offset_data_block ::UInt64
 end
 
 function MetDenseFile( filename ::String )
-    f = open( "test.metdense" )
+    f = open( filename )
 
     # Read and check magic string
     s = Vector{UInt8}(undef,8)
     readbytes!( f, s )
     @assert s == Vector{UInt8}("MetDense")
-    
+
     # Read version
     version = ( read( f, UInt32 ), read( f, UInt32 ) )
-    @assert version == ( 0, 0 )
-    
+    @assert version == ( 0, 1 )
+
     # Get offsets
-    offset_data_block = read( f, UInt32 )
-    offset_chroms_block = read( f, UInt32 )
-    
+    offset_data_block = read( f, UInt64 )
+    offset_chroms_block = read( f, UInt64 )
+
     # Read Cells block
     n_cells = read( f, UInt32 )
     names_cells = [ readline( f ) for i in 1:n_cells ]
-    
+
     # Read Chromosomes block
     seek( f, offset_chroms_block )
     n_chroms = read( f, UInt32 )
-    offsets_chroms = [ read( f, UInt32 ) for i in 1:n_chroms ]
+    offsets_chroms = [ read( f, UInt64 ) for i in 1:n_chroms ]
     names_chroms = [ readline( f ) for i in 1:n_chroms ]
 
-    chroms_dict = Dict( names_chroms[i] => 
-        range( offsets_chroms[i], 
-            i < n_chroms ? offsets_chroms[i+1]-1 : offset_chroms_block-1 )
+    chroms_dict = Dict( names_chroms[i] =>
+        range( offsets_chroms[i],
+            i < n_chroms ? offsets_chroms[i + 1] - 1 : offset_chroms_block - 1;
+            step = 4 * ceil(UInt64, n_cells / 16))
         for i = 1:n_chroms )
-    
+
     MetDenseFile( f, names_cells, chroms_dict, offset_data_block )
 end
 
@@ -63,11 +63,11 @@ function read_at_position( f, pos, type )
 end
 
 function get_position( mdf ::MetDenseFile, gi:: GenomicInterval )
-    from = searchsortedfirst( 
-        mdf.chroms_dict[ gp.chrom ], gp.pos, 
+    from = searchsortedfirst(
+        mdf.chroms_dict[ gp.chrom ], gp.pos,
         by = ( x -> read_at_position( mdf.f, x, UInt32 ) ) )
-     to = searchsortedlast( 
-        mdf.chroms_dict[ gp.chrom ], gp.pos, 
+     to = searchsortedlast(
+        mdf.chroms_dict[ gp.chrom ], gp.pos,
         by = ( x -> read_at_position( mdf.f, x, UInt32 ) ) )
     range( from, to )
 end
@@ -84,3 +84,6 @@ end
 main()
 
 gp = GenomicPosition( "2", 1234567 )
+
+df = MetDenseFile("data/test.metdense")
+df.chroms_dict
