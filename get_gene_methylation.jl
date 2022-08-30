ce0c202b693803509d7ba6bc9929575c99e688ca
Jooa <code@jooa.xyz>
2022-08-30 16:45:31 +0200

    Restructure files
    Remove most superfluous files.
    Move files with important code to the src directory

--- get_gene_methylation.jl
+++
@@ -1,7 +0,0 @@
-#read metdense file to get average methylation for each gene and cell
-using CSV
-using DataFrames
-
-allGenes = CSV.File("data/all_genes.txt") |> DataFrame
-
-sum((allGenes.tss .!= allGenes.start) .& (allGenes.tss .!= allGenes.fin))
